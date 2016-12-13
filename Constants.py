9a614a5d31267ed516ae8809c0953ac0c2c10488
moraw <krzymoraw@gmail.com>
2016-12-13 13:23:17 +0100

    Corrected constant values for Lorentz image series

--- Constants.py
+++ Constants.py
@@ -1,14 +1,14 @@
 serName = 'd'
 nFiles = 20
 dimSize = 1024
-numInFocus = 8
+numInFocus = 10
 idxInFocus = numInFocus - 1
 refIdx = 10
 predefDfStep = 2.0e-9
-nIterations = 3
-pxWidth = 41.0289e-12
+nIterations = 10
+pxWidth = 286.515e-12
 ewfLambda = 1.97e-12
-dfStepMin, dfStepMax, dfStepChange = 0.002, 0.004, 0.001
+dfStepMin, dfStepMax, dfStepChange = 0.4, 1.2, 0.02
 ccWidgetDim = 512
 gridDim = 3
 
