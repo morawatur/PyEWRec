d45a46bc5e011c5a35b0d89d0dedb6b63cb52185
moraw <krzymoraw@gmail.com>
2017-04-10 17:36:35 +0200

    Added functions for checking used and available GPU memory

--- CudaConfig.py
+++ CudaConfig.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numba import cuda
 
 # -------------------------------------------------------------------
 
@@ -13,3 +14,17 @@ def DetermineCudaConfigNew(dims):
     blockDim = 32, 32
     gridDim = np.ceil(dims[0] / blockDim[0]).astype(np.int32), np.ceil(dims[1] / blockDim[1]).astype(np.int32)
     return blockDim, gridDim
+
+# -------------------------------------------------------------------
+
+def GetGPUMemoryUsed():
+    freeMem, totalMem = cuda.current_context().get_memory_info()
+    gpuMemUsedInMBs = (totalMem - freeMem) / (1024.0 ** 2)
+    print('GPU memory used = {0:.2f} MB'.format(gpuMemUsedInMBs))
+
+# -------------------------------------------------------------------
+
+def GetGPUFreeMemory():
+    gpuFreeMemInBytes = cuda.current_context().get_memory_info()[0]
+    gpuFreeMemInMBs = gpuFreeMemInBytes / (1024.0 ** 2)
+    print('Available GPU memory = {0:.2f} MB'.format(gpuFreeMemInMBs))
